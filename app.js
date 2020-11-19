a3678ca87b42dde3fd31ca4e6ead0ce850a73e0f
Soslan Aldatov <me@sptm.dev>
2020-11-20 00:34:54 +0500

    add getInviteData() method to extract data from invites in useful format

--- app.js
+++ app.js
@@ -4,7 +4,41 @@ const pino = require('pino');
 const Srf = require('drachtio-srf');
 const srf = new Srf() ;
 const logger = srf.locals.logger = pino();
+
+const convert = require('xml-js');
+
+const util = require('util');
+
+
 let callHandler;
+
+getInviteData = (req) => {
+  const data = {};
+  data.invite = {};
+
+  data.invite.via = req.get('Via');
+  data.invite.from = req.get('From');
+  data.invite.to = req.get('To');
+  data.invite.cseq = req.get('cseq');
+  data.invite.contact = req.get('Contact');
+  data.invite.subject = req.get('Subject');
+
+  const sdp_payload = req.payload[0];
+  const metadata_payload = req.payload[1];
+
+  data.sdp_object = {};
+  data.metadata_object = {};
+
+  data.sdp_object.type = sdp_payload.type || false;
+  data.sdp_object.content = sdp_payload.content || false;
+
+  data.metadata_object.type = metadata_payload.type || false;
+  data.metadata_object.content = false;
+  data.metadata_object.content = convert.xml2js(metadata_payload.content, {compact: true, spaces: 4, nativeType: true, nativeTypeAttributes: true}) || false;
+
+  return data;
+}
+
 
 if (config.has('drachtio.host')) {
   logger.info(config.get('drachtio'), 'attempting inbound connection');
@@ -24,11 +58,16 @@ if (config.has('rtpengine')) {
 
   // we only want to deal with siprec invites (having multipart content) in this application
   srf.use('invite', (req, res, next) => {
+
     const ctype = req.get('Content-Type') || '';
     if (!ctype.includes('multipart/mixed')) {
       logger.info(`rejecting non-SIPREC INVITE with call-id ${req.get('Call-ID')}`);
       return res.send(488);
+    } else {
+      const inviteData = getInviteData(req);
+      console.log(util.inspect(inviteData, false, null))
     }
+
     next();
   });
 
